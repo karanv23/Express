8febd958c1c97c2e5701e173b124d3872f34bc7d
karanv23 <72219361+karanv23@users.noreply.github.com>
2020-11-30 07:38:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const ExpressClient = require('./Structures/ExpressClient.js');
 const config = require('../config');
 
+client.on('ready', () => {
+  client.user.setActivity('CGX HΩMΞTΩWN™');
+})
+
 const client = new ExpressClient(config);
 client.start();
